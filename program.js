db3979b14dd5ee5b29cf05b5d86068588c130881
Manthan Patel <manthanpl93@live.com>
2021-02-22 10:16:16 +0530

    Array some() Method

--- program.js
+++ program.js
@@ -13,3 +13,30 @@
   
   const coppiedPerson = { ...person };
 }
+
+
+{
+  /* Array some() Method  */
+  
+  const users = [
+  { username: "manthan", role: "admin" },
+  { username: "mayank", Role: "user" },
+ ];
+
+const userIsAdmin = (userName) =>
+  users.some((user) => user.username === userName && user.role === "admin");
+
+const userName1 = "manthan";
+console.log(`${userName1} is Admin: ${userIsAdmin(userName1)}`);
+const userName2 = "mayank";
+console.log(`${userName2} is Admin: ${userIsAdmin(userName2)}`);
+const userName3 = "rahul";
+console.log(`${userName3} is Admin: ${userIsAdmin(userName3)}`);
+/*
+  Output:
+  manthan is Admin: true  
+  mayank is Admin: false
+  rahul is Admin: false
+*/
+  
+}
